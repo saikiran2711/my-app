330374bc3abc9f947bf7ad3e1569b4073807f35a
Sai Kiran <saikirankavali1010@gmail.com>
2020-07-18 14:56:57 +0530

    SPA-!

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import {DISHES } from '../shared/dishes';
-import DishDetail from './DishdetailComponent';
+// import DishDetail from './DishdetailComponent';
 import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from "./ContactComponent";
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component{
     constructor(props)
@@ -14,18 +18,23 @@ class Main extends Component{
 
       this.state={
         dishes:DISHES,
-        selectedDish:null
+        // selectedDish:null
+        comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
       };
     }
 
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish });
-    }
+    // onDishSelect(dish) {
+    //     this.setState({ selectedDish: dish });
+    // }
   render() {
 
     const HomePage = () => {
       return(
-          <Home 
+          <Home  dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -37,6 +46,7 @@ class Main extends Component{
     <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
     <Footer />
